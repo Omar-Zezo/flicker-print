fd7235a69affbb23b0fc158a63ca1370880be7f8
Omar-Zezo <omarabdelazizmohamed408@gmail.com>
2025-01-03 22:47:10 +0200

    flicker

--- src/pages/policies/ContactUs.jsx
+++ src/pages/policies/ContactUs.jsx
@@ -1,7 +1,7 @@
 import Navigation from "@/utils/Navigation";
 import UseLangDetection from "@/hooks/UseLangDetection";
 import Header from "@/utils/Header";
-import ContactUsContainer from "@/components/ContactUs/contactUsContainer";
+import ContactUsContainer from "@/components/contactUs/contactUsContainer";
 
 
 const ContactUs = () => {
